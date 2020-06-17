5a5db10deacffe594eba88fe5144c7ac0078e554
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-06-17 23:42:55 +0700

    install socket.io

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import io from "socket.io-client";
 import Layout from "../components/layout";
 import Card from "../components/card";
 
@@ -6,11 +7,12 @@ export default function Home() {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
+    io("/"); // for register to websocket
     fetch("/api/videos")
       .then((res) => res.json())
       .then((data) => setVideos(data.data));
   }, []);
-  
+
   return (
     <Layout>
       <div className="row">
--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const { createServer } = require("http");
-const { parse } = require("url");
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 const next = require("next");
 
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
+// socket.io
+io.on("connection", () => {
+  console.log("new connection");
+});
 
-    if (pathname === "/a") {
-      app.render(req, res, "/a", query);
-    } else if (pathname === "/b") {
-      app.render(req, res, "/b", query);
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(3000, (err) => {
+nextApp.prepare().then(() => {
+  app.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
     if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
